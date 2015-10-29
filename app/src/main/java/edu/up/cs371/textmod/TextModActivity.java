90262a8d827488cf5222ef8360b9e658cee5e745
Abhinav <abhinav.abhi96@gmail.com>
2015-10-28 17:01:52 -0700

    Abhinav's changes, alternating case

--- app/src/main/java/edu/up/cs371/textmod/TextModActivity.java
+++ app/src/main/java/edu/up/cs371/textmod/TextModActivity.java
@@ -33,6 +33,7 @@ public class TextModActivity extends ActionBarActivity {
     //instance variable containing text
     private EditText text;
     private Spinner spinner;
+    //hello
 
     /**
      * @see android.app.Activity#onCreate(android.os.Bundle)
